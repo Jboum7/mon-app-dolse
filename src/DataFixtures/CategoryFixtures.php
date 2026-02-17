b53dee9a1fa84d2d23424ed91078a78dddffb358
Jboum7 <obenseghir91@gmail.com>
2026-02-17 14:22:59 +0100

    remove folder uploads/products when use fixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -10,12 +10,18 @@ class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 10; $i++) {
+        $categories = ['T-shirt', 'Pantalon', 'Chaussure', 'Chaussettes', 'Sous-vêtement', 'Accessoire'];
+
+        foreach ($categories as $key => $value) {
             $category = new Category();
 
-            $category->setTitle("Category . $i");
+            $category->setTitle($value);
+
             $manager->persist($category);
+            $this->addReference('category-' . $key, $category);
         }
+
         $manager->flush();
     }
 }
+
---
+++ src/DataFixtures/ProductFixtures.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+use Smknstd\FakerPicsumImages\FakerPicsumImagesProvider;
+
+class ProductFixtures extends Fixture implements DependentFixtureInterface
+{
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create('fr_FR');
+        $faker->addProvider(new FakerPicsumImagesProvider($faker));
+
+        $destDir = dirname(__DIR__) . '/../public/uploads/products';
+
+        if (!is_dir($destDir)) {
+            mkdir($destDir, 0775, true);
+        } else {
+            exec('rm -rf ' . $destDir);
+            mkdir($destDir, 0775, true);
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            $product = new Product();
+            $filePath = $faker->image(dir: '/tmp');
+
+            if ($filePath) {
+                $ext = pathinfo($filePath, PATHINFO_EXTENSION);
+                $filename = uniqid('products_', true) . '.' . $ext;
+
+                copy($filePath, $destDir . '/' . $filename);
+                $product->setImageFilename($filename);
+            }
+
+            $product->setTitle($faker->words(3, true))
+                ->setPrice($faker->numberBetween($min = 50, $max = 300))
+                ->setDescription($faker->realText($maxNbChars = 200, $indexSize = 2))
+                ->setCategory($this->getReference('category-' . rand(0, 5), Category::class));
+
+            $manager->persist($product);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            CategoryFixtures::class,
+        ];
+    }
+}
